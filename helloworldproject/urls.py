aa1631587d3dcd2f1b5dc47adc92e1d495518cff
CHINATSU <isen99068@gmail.com>
2021-07-08 00:54:53 +0900

    add blogproject

--- helloworldproject/urls.py
+++ helloworldproject/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from .views import helloworldfunc,HelloWorldClass
+from django.urls import path, include
+#from .views import helloworldfunc,HelloWorldClass
 """
 ブラウザから来たrequest とpath()内のオレンジ文字に書かれたURLに合致したさいに、view.pyにかかれている中身の
 class,functionを実行するように指示をする
 """
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('helloworldurl/', helloworldfunc),
-    path('helloworldurl2/',HelloWorldClass.as_view()),
+    #path('helloworldurl/', helloworldfunc),
+    #path('helloworldurl2/',HelloWorldClass.as_view()),
+    path('',include('helloworldapp.urls')),
 ]
